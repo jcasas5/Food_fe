fdf95a561de7d7bfb03b73cf9c61c6f72fba2e02
jcasasus5 <jcasasus.ieu2020@student.ie.edu>
2022-12-19 16:22:39 +0100

    op

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import AppRecipes from '../components/AppRecipes.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter)  
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'AppRecipes',
     component: AppRecipes
-  },
-
+  }
 ]
 
 const router = new VueRouter({
